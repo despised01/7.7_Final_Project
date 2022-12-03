49ebe18e3e8e525b8831469d2a0cc77aeb571fd5
Антон Левин <despised01@gmail.com>
2022-12-03 22:56:55 +0300

    Добавьте файлы проекта.

---
+++ 7.7_Final_Project/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _7._7_Final_Project
+{
+    abstract class Delivery
+    {
+        public string Address;
+    }
+
+    class HomeDelivery: Delivery
+    {
+
+    }
+
+    class PickPointDelivery: Delivery
+    {
+
+    }
+
+    class ShopDelivery: Delivery
+    {
+
+    }
+
+    class Order <TDelivery, TStruct> where TDelivery: Delivery
+    {
+        public TDelivery Delivery;
+
+        public int Number;
+
+        public string Description;
+
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+
+    }
