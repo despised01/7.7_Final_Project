755ab6fc4be5c6df38382f19e7495452fdaafdd5
Антон Левин <despised01@gmail.com>
2022-12-07 20:30:19 +0300

    Добавлены элементы композиции в класс "Courier"

--- 7.7_Final_Project/Program.cs
+++ 7.7_Final_Project/Program.cs
@@ -2,27 +2,50 @@
 
 namespace _7._7_Final_Project
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Итоговое задание по ООП");
+
+            Console.WriteLine(DisplayAddress());
+        }
+    }
     abstract class Delivery
     {
         public string Address;
+        
+        protected virtual void DisplayStatus()
+        {
+            Console.WriteLine("Доставка");
+        }
     }
 
-    class HomeDelivery: Delivery
+    class HomeDelivery : Delivery
     {
-
+        protected override void DisplayStatus()
+        {
+          Console.WriteLine("Доставка на дом");
+        }
     }
 
-    class PickPointDelivery: Delivery
+    class PickPointDelivery : Delivery
     {
-
+        protected override void DisplayStatus()
+        {
+           Console.WriteLine("Доставка в пункт выдачи");
+        }
     }
 
-    class ShopDelivery: Delivery
+    class ShopDelivery : Delivery
     {
-
+        protected override void DisplayStatus()
+        {
+            Console.WriteLine("Доставка в магазин");
+        }
     }
 
-    class Order <TDelivery, TStruct> where TDelivery: Delivery
+    class Order<TDelivery, TStruct> where TDelivery : Delivery
     {
         public TDelivery Delivery;
 
@@ -34,5 +57,79 @@ namespace _7._7_Final_Project
         {
             Console.WriteLine(Delivery.Address);
         }
-
     }
+
+    class Person
+    {
+        public string Name;
+        public string Surname;
+    }
+
+    class Customer: Person
+    {
+        private string PhoneNumber;
+        private int age;
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                if (value < 18)
+                {
+                    Console.WriteLine("Для совершения заказа возраст должен быть не меньше 18 лет");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+
+        static bool CheckPhone(string phone, out int corrphone)
+        {
+            if (int.TryParse(phone, out int intphone))
+            {
+                if (intphone > 0)
+                {
+                    corrphone = intphone;
+                    return false;
+                }
+            }
+            {
+                corrphone = 0;
+                return true;
+            }
+        }
+
+        public Customer(string name, string surname, string phonenumber, int age)
+        {
+            Name = name;
+            Surname = surname;
+            PhoneNumber = phonenumber;
+            Age = age;
+        }
+    }
+
+    class Courier : Person
+    {
+        private HomeDelivery homeDelivery;
+
+        private PickPointDelivery pickPointDelivery;
+
+        private ShopDelivery shopDelivery;
+
+        public Courier()
+        {
+            homeDelivery = new HomeDelivery();
+
+            pickPointDelivery = new PickPointDelivery();
+
+            shopDelivery = new ShopDelivery();
+        }
+    }
+}
